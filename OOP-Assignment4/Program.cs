e65245f3441e3ee17319338ff2a80575abfaa3c3
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-08-14 22:52:45 +0300

    Theoretical Questions

--- OOP-Assignment4/Program.cs
+++ OOP-Assignment4/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello_world");
+            //theoretical
+            #region Question1-Abstraction
+            //a)  What is Abstraction in Object-Oriented Programming?
+            //sol:  Abstraction means hiding unnecessary details showing only the important parts
+
+            //b)  Why is abstraction considered one of the four pillars of OOP ?
+            //sol: It is one of the four pillars of OOP because it helps us focus on what an object does
+            #endregion
+
+            #region Question2
+
+            //a)  What is the difference between an Abstract Class and an Interface?
+            //sol: Abstract Class can have fields - constructors - methods and abstract methods
+            // Interface defines a contract that a class must follow
+
+            //b)  When would you choose an Interface instead of an Abstract Class ?
+            //sol: We choose an Interface when different classes me behavior
+
+            //c)  Can a class inherit from multiple abstract classes? Can it implement multiple interfaces?
+            //sol: A class cannot inherit from multiple abstract classes  But it can implement multiple interfaces
+
+            #endregion
+
         }
     }
 }
